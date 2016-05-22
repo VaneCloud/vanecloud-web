33e132be99c693ca099343d71ee274c83a53bcbd
Saw85 <2804265880@qq.com>
2016-05-22 12:57:44 +0800

    add APP

---
+++ AppInvite/js/job.js
@@ -0,0 +1,32 @@
+	var zkqy=angular.module('zkqy',[]);
+
+	zkqy.controller('work',['$scope',function($scope){
+    	$scope.worksList = [
+        	{
+            	job:'云计算工程师',
+
+            	duty:[
+				'1. 参与云平台相关的调研、开发和测试；',
+				'2. 参与云平台的部署和维护；',
+				'3. 编写、修订和审核技术支持相关文档和作业指导书。',
+            	],
+
+            	require:[
+			    '1. 计算机相关专业；',
+			    '2. 最少熟悉一种Hypervisor平台（如VMware,KVM等）；',
+			    '3. 熟练至少一种脚本语言，熟悉python的优先；',
+			    '4. 熟练操作Linux操作系统；',
+			    '5. 熟悉OpenStack产品及各个组件优先；',
+			    '6. 有自动化运维经验，例如使用chef，puppet，ansible等自动运维软件经验的优先；',
+			    '7. 拥有一定的英语读写听说能力。'
+                ],
+
+                site:'上海'
+           	},
+
+           	
+        ];
+
+    }]);
+
+			
---
+++ AppInvite/js/pic.js
@@ -0,0 +1,72 @@
+$(function(){
+	var oPicList=document.getElementById("carousel-inner");
+	var aBtns=document.getElementById("picBtns").children;
+	var jobPic=document.getElementById("jobPic");
+	var iScroll=0;
+	var iStartX=0;
+	var iStartPageX=0;
+	var iNow=0;
+	var oTimer=0;
+	oPicList.innerHTML+=oPicList.innerHTML;
+	oPicList.style.width=oPicList.clientWidth*2+"px";
+	function autoPlay()
+	{
+		oTimer=setInterval(function(){
+			iNow++;	
+			next();
+		},3000);
+	}
+	autoPlay();
+	oPicList.addEventListener("touchstart",fnStart,false);
+	function fnStart(ev)
+	{
+		clearInterval(oTimer);
+		clearInterval(oPicList.timer);
+		if(iNow<=0)
+		{
+			iNow+=aBtns.length;
+			iScroll=-iNow*jobPic.offsetWidth;
+			css(oPicList, "translateX", iScroll);
+		}
+		iStartPageX=ev.changedTouches[0].pageX;
+		iStartX=iScroll;
+	};
+	oPicList.addEventListener("touchmove",fnMove,false);
+	function fnMove(ev)
+	{
+		var iDis=ev.changedTouches[0].pageX-iStartPageX;
+		iScroll=iStartX+iDis;
+		css(oPicList, "translateX", iScroll);
+	};
+	oPicList.addEventListener("touchend",fnEnd,false);
+	function fnEnd(ev)
+	{
+		var iDis=ev.changedTouches[0].pageX-iStartPageX;
+		var iNub=Math.round(iDis/jobPic.offsetWidth);
+		iNow-=iNub;
+		next();
+		autoPlay();
+	};
+	function next()
+	{
+		iScroll=-iNow*jobPic.offsetWidth;
+		for(var i=0;i<aBtns.length;i++)
+		{
+			aBtns[i].className="";
+		}
+		aBtns[iNow%aBtns.length].className="active";
+		if(iNow>=aBtns.length)
+		{
+			tweenMove(oPicList,{translateX:iScroll},300,"easeOut",function(){
+				iNow=iNow%aBtns.length;
+				iScroll=-iNow*jobPic.offsetWidth;
+				css(oPicList, "translateX", iScroll);
+			});
+		}
+		else
+		{
+			tweenMove(oPicList,{translateX:iScroll},300,"easeOut");
+		}
+	}
+	
+})
